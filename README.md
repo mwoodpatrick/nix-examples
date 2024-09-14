45aa128c1180a80cc43ed991732e279ff3e4dc44
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-09-14 18:59:03 +0000

    Add expression examples

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # nix-examples
 A simple set of nix examples &amp; experiments
+
+# Nix Repl
+
+[Nix Pills](https://nixos.org/guides/nix-pills/00-preface)
+[Nix Reference Manual](https://nix.dev/manual/nix/2.24/command-ref/new-cli/nix3-repl)
+
+# Overlays
+
+See [Mastering Nixpkgs Overlays: Techniques and Best Practice](https://nixcademy.com/posts/mastering-nixpkgs-overlays-techniques-and-best-practice/)
+
+directory simple-overlay
+
+In the REPL:
+```
+
+$ nix repl
+
+nix-repl> map (x: x * 2) [1 2 3]
+    [ 2 4 6 ]
+
+pkgs = import ~/src/nixpkgs { 
+  overlays = [
+    someOverlayFunction
+  ];
+  config = { };
+}
+```
---
+++ repl/expressions.nix
@@ -0,0 +1,47 @@
+# [Nix Pills](https://nixos.org/guides/nix-pills/00-preface)
+
+rec {
+# Nix expressions
+
+
+a = 3;
+b = 4;
+
+# If expressions
+c = if a > b then "yes" else "no";
+
+# Let expressions
+l = let a = "foo"; in a ;
+
+# With expression
+longName = { a = 3; b = 4; };
+
+e = longName.a + longName.b;
+w = with longName; a + b;
+
+# More than one parameter
+
+mul = a: (b: a*b);
+
+# default arguments
+mul2 = { a, b ? 2 }: a*b;
+
+g = mul2 { a = 9; };
+
+h = mul2 { a=9; b=4; };
+
+# variadic arguments
+
+mul3 = s@{ a, b, ... }: a*b*s.c;
+v = mul3 { a = 3; b = 4; c = 2; };
+
+mul4 = { a, b, ... }@s: a*b*s.c;
+z = mul4 { a = 3; b = 4; c = 2; };
+
+# imports
+
+ia = import ./a.nix;
+ib = import ./b.nix;
+imul = import ./mul.nix;
+ires = imul a b;
+}
