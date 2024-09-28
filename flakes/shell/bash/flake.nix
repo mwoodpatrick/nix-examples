7783f4dc742d0743d46a1be140fd5d440fd323a8
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-09-28 06:22:55 -0700

    Add some more docs to explain flow better

--- flakes/shell/bash/flake.nix
+++ flakes/shell/bash/flake.nix
@@ -1,14 +1,21 @@
+# [Flakes - nixos.wiki](https://nixos.wiki/wiki/Flakes)
+# [The nix flake user manual](https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-flake.html)
+# [The nix flake references manual](https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-flake)
 # [Official Nix templates](https://github.com/NixOS/templates)
 # nix flake show templates
 # nix flake init -t templates#bash-hello
 # git add flake.nix # required otherwise it will not build
 # nix build
+# nix flake check
 {
   description = "An over-engineered Hello World in bash";
 
   # Nixpkgs / NixOS version to use.
+  # [The nix flake inputs manual](https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-flake.html#flake-inputs)
   inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
 
+# self - the flake being created
+# evaluated inputs 
   outputs = { self, nixpkgs }:
     let
 
@@ -22,6 +29,10 @@
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      # [genAttrs - reference](https://ryantm.github.io/nixpkgs/functions/library/attrsets/#function-library-lib.attrsets.genAttrs)
+      # Takes a list of attribute names and a function that maps names to values and returns 
+      # an attribute set mapping specified names to mapped values
+      # In this case the created attribute set maps system names to the package for that system
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
@@ -32,6 +43,7 @@
     {
 
       # A Nixpkgs overlay.
+      # [Overlays - nixos.wiki](https://nixos.wiki/wiki/Overlays)
       overlay = final: prev: {
 
         hello = with final; stdenv.mkDerivation rec {
@@ -69,8 +81,10 @@
       defaultPackage = forAllSystems (system: self.packages.${system}.hello);
 
       # A NixOS module, if applicable (e.g. if the package provides a system service).
+      # [NixOS modules](https://nixos.wiki/wiki/NixOS_modules)
+      # Nix Modules are used to extend Nix OS
       nixosModules.hello =
-        { pkgs, ... }:
+        { config, pkgs, ... }:
         {
           nixpkgs.overlays = [ self.overlay ];
 
