26978d5894b3595cb838a27b649f601e20dd91ef
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-10-22 15:02:31 -0700

    Add some debugging info

--- vm-examples/minimal/flake.nix
+++ vm-examples/minimal/flake.nix
@@ -1,5 +1,8 @@
+# [Debugging with nix repl](https://nixos-and-flakes.thiscute.world/best-practices/debugging#debugging-with-nix-repl)
+# [Learn how to use the Nix REPL effectively](https://aldoborrero.com/posts/2022/12/02/learn-how-to-use-the-nix-repl-effectively)
 # [Modularize Your NixOS Configuration](https://nixos-and-flakes.thiscute.world/nixos-with-flakes/modularize-the-configuration)
 # [NixOS "Stoat" 23.05](https://www.youtube.com/watch?v=79-O7sQxHwk)
+# [mpickering/configuration.nix](https://gist.github.com/mpickering/a3be96e06105e58355b5659dffb8c47e)
 # [NixOS/nixpkgs](https://github.com/NixOS/nixpkgs.git)
 # [mwoodpatrick/nixpkgs](https://github.com/mwoodpatrick/nixpkgs.git)
 # [nix-community/home-manager](https://github.com/nix-community/home-manager)
@@ -30,5 +33,6 @@
         modules = [ ./vm.nix ];
         # modules = [ ./nixos/configuration.nix ];
       };
+      inherit inputs;
     };
 }
