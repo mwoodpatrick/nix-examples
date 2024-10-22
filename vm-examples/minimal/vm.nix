59af71d973a270fef4c615db0bbd305e3cf4c293
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-10-21 17:04:48 -0700

    more settings

--- vm-examples/minimal/vm.nix
+++ vm-examples/minimal/vm.nix
@@ -5,7 +5,60 @@
 # Then connect with ssh -p 2222 guest@localhost
 { inputs, lib, config, pkgs, ... }:
 {
-  # nix.nixPath = [ "nixpkgs=/home/matt/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels" ];
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    # ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # nixpkgs.config.allowUnfree = true;
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    # nixPath = [ "nixpkgs=/home/matt/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels" ];
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -56,6 +109,9 @@
         firefox
         # thunderbird
         ];
+    openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
   };
 
   security.sudo.wheelNeedsPassword = false;
@@ -76,8 +132,20 @@
   # Enable ssh
   services.sshd.enable = true;
 
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  # services.openssh = {
+  #   enable = true;
+  #   settings = {
+  # Opinionated: forbid root login through SSH.
+  #     PermitRootLogin = "no";
+  # Opinionated: use keys only.
+  # Remove if you want to SSH using passwords
+  #     PasswordAuthentication = false;
+  #  };
+  # };
+
   # Included packages in system profile here
-  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     dig
     fzf
