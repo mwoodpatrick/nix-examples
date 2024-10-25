65e210d95bf33d084f54319e82dc6467a654be82
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-10-25 16:02:33 -0700

    Added more users and mountd /mnt/wsl/projects as /mnt/projects (currently writing to volume does not persist)

--- vm-examples/micro-vm/flake.nix
+++ vm-examples/micro-vm/flake.nix
@@ -1,3 +1,5 @@
+# Build this VM with: nixos-rebuild build-vm --flake .#my-microvm
+# Then rim it with: nix run .#my-microvm
 {
   description = "NixOS in MicroVMs";
 
@@ -27,7 +29,43 @@
             microvm.nixosModules.microvm
             {
               networking.hostName = "my-microvm";
-              users.users.root.password = "";
+
+
+  # A default user able to use sudo
+  users.users = {
+    root.password = "";
+    guest = {
+      isNormalUser = true;
+      home = "/home/guest";
+      extraGroups = [ "wheel" ];
+      initialPassword = "westie";
+      # packages = [ inputs.home-manager.packages.${pkgs.system}.default ];
+      # packages = with pkgs; [
+      #   firefox
+      #   # thunderbird
+      #   ];
+      openssh.authorizedKeys.keys = [
+          # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        ];
+    };
+
+    alice = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      initialPassword = "westie";
+    };
+
+    admin = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      initialPassword = "westie";
+    };
+  };
+
+security.sudo.wheelNeedsPassword = false;
+
+              # Set your time zone.
+              time.timeZone = "America/Los_Angeles";
               microvm = {
                 volumes = [ {
                   mountPoint = "/var";
@@ -42,7 +80,19 @@
                   # squashfs/erofs will be built for it.
                   source = "/nix/store";
                   mountPoint = "/nix/.ro-store";
-                } ];
+                }
+
+                {
+                  # use proto = "virtiofs" for MicroVMs that are started by systemd
+                  proto = "9p";
+                  tag = "projects";
+                  # a host's /nix/store will be picked up so that no
+                  # squashfs/erofs will be built for it.
+                  source = "/mnt/wsl/projects";
+                  mountPoint = "/mnt/projects";
+                }
+
+                ];
 
                 # "qemu" has 9p built-in!
                 hypervisor = "qemu";
