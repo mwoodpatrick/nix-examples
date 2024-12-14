3f98f02c54acb738214e1f780cd1e7014bf3a4b7
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-14 04:26:59 -0800

    A flake that has no inputs and generates no outputs

---
+++ flakes/minimal/null_flake/flake.nix
@@ -0,0 +1,11 @@
+# A very minimal flake
+# nix flake metadata .
+#   Resolved URL:  git+file:///mnt/wsl/projects/git/nix-examples?dir=flakes/minimal/null_flake
+#   Description:   Some description
+#   Path:          /nix/store/lfgac0pkyn3m58n6f5vaj042whw3p80v-source
+#   Revision:      2c35d1edc6822be0755e59cfba56b7ccd631e2b6-dirty
+#   Last modified: 2024-12-12 16:36:45
+{
+  description = "Some description";
+  outputs = _: { };
+}
