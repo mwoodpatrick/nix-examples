3ca5c8e62793e235086bafa2b0abc9c708a4d7b2
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-18 15:19:43 -0800

    init simple vm test

---
+++ flakes/vmtests/debian/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-vm-test.url = "github:numtide/nix-vm-test";
+  };
+
+  outputs = { self, nixpkgs, nix-vm-test }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
