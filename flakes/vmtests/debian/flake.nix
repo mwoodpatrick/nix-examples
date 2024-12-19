845832bf3ab6844dc53f121a092024b8a68c21fd
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-18 16:23:39 -0800

    Add simple test

--- flakes/vmtests/debian/flake.nix
+++ flakes/vmtests/debian/flake.nix
@@ -1,3 +1,4 @@
+# [Getting Started - nix-vm-test](https://github.com/numtide/nix-vm-test/blob/main/doc/getting-started.md)
 {
   description = "A very basic flake";
 
@@ -10,7 +11,24 @@
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+    packages.x86_64-linux.test-vm =
+      let
+        vmTest = nix-vm-test.lib.x86_64-linux.debian."13" {
+          sharedDirs = {
+            debDir = {
+              source = "${./.}";
+              target = "/mnt/debdir";
+            };
+          };
+          testScript = ''
+            vm.wait_for_unit("multi-user.target")
+            vm.succeed("apt-get -yq install /mnt/debdir/hello.deb")
+          '';
+        };
+        in vmTest.driver; # vmTest.driverInteractive;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.test-vm;
   };
 }
