5b35cc8579e9a87dd30469a8756b88a86384dfde
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-18 16:46:40 -0800

    Add some comments and an additional test

--- flakes/vmtests/debian/flake.nix
+++ flakes/vmtests/debian/flake.nix
@@ -2,6 +2,8 @@
 {
   description = "A very basic flake";
 
+  # Load the dependencies
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-vm-test.url = "github:numtide/nix-vm-test";
@@ -13,17 +15,33 @@
 
     # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+    # Create a test for Debian 13
     packages.x86_64-linux.test-vm =
       let
         vmTest = nix-vm-test.lib.x86_64-linux.debian."13" {
+
+          # This makes the current folder available in the test at /mnt/debdir
+
           sharedDirs = {
             debDir = {
               source = "${./.}";
               target = "/mnt/debdir";
             };
           };
+
+          # A synthetic test
+
           testScript = ''
+            # Wait for the system to be fully booted
+
             vm.wait_for_unit("multi-user.target")
+
+            # Test that the mount worked
+
+            vm.succeed('ls /mnt/debdir')
+
+            # Test that the package installs
+
             vm.succeed("apt-get -yq install /mnt/debdir/hello.deb")
           '';
         };
