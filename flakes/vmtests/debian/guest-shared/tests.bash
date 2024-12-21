a4e89bdc94960dba0346fbb656d980a6509bba5b
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-21 11:49:56 -0800

    Fix test for NixOS

--- flakes/vmtests/debian/guest-shared/tests.bash
+++ flakes/vmtests/debian/guest-shared/tests.bash
@@ -6,42 +6,50 @@ function check_command {
     cmd=$1
     pat=$2
 
-    cmd_output.txt = $( $cmd ) &&
+    # echo "checking for pattern $2"
+
+    $cmd > cmd_output.txt &&
     cat cmd_output.txt &&
-    if [ grep -v "$pat" cmd_output.txt ]]; then
+
+    if grep -q "$pat" cmd_output.txt; then
         echo "Expected output found for distro $DISTRO"
         return 0
-    else 
+    else
         echo "Expected output missing for distro $DISTRO"
         return 1
     fi
 }
 
-function install_packages {
+function run_tests {
+    local pat="Hello, world!"
+    check_command hello "$pat"
+}
+
+function install_packages_and_run_tests {
     # Run commands based on the detected distro
     case $DISTRO in
         ubuntu|debian)
             echo "Running commands for Debian-based systems (Ubuntu, Debian)..."
-            apt update &&
+            # apt update &&
             apt install -y $package
             # Add your Debian-based commands here
             apt-get -yq install /mnt/host-shared/hello.deb
             ;;
         fedora|centos|rhel)
             echo "Running commands for Red Hat-based systems (Fedora, CentOS, RHEL)..."
-            dnf update -y
+            # dnf update -y
             dnf install -y $package
             # Add your Red Hat-based commands here
             ;;
         arch|manjaro)
             echo "Running commands for Arch-based systems (Arch, Manjaro)..."
-            pacman -Syu
+            # pacman -Syu
             pacman -S --noconfirm $package
             # Add your Arch-based commands here
             ;;
         nixos )
             echo "Running commands for NixOS-based systems ..."
-            nix-shell -p hello
+            nix-shell -p hello --run "source ${BASH_SOURCE[0]} && run_tests"
 
             # Add your NixOS-based commands here
             ;;
@@ -50,10 +58,6 @@ function install_packages {
             exit 1
             ;;
     esac
-}
-
-function run_tests {
-    check_command hello "Hello World!"
 }
 
 # Detect the Linux distribution
