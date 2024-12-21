8a4fe54e60cd4b0c576be8d7cb0aa4559bdbf7c5
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2024-12-21 08:35:17 -0800

    Add some more outputs

--- flakes/vmtests/debian/flake.nix
+++ flakes/vmtests/debian/flake.nix
@@ -1,6 +1,7 @@
 # [Getting Started - nix-vm-test](https://github.com/numtide/nix-vm-test/blob/main/doc/getting-started.md)
 # [nix-vm-test](https://github.com/numtide/nix-vm-test)
 #
+# [nix build](https://nix.dev/manual/nix/2.25/command-ref/new-cli/nix3-build)
 # [nix run](https://nix.dev/manual/nix/2.25/command-ref/new-cli/nix3-run)
 # nix run .#hello
 # nix run .#test-vm
@@ -23,15 +24,16 @@
       let
         system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
         distro = "debian";
         versions = {
             debian = ["12" "13"];
             ubuntu = ["23_10" "22_04"];
-            fedora = ["38" "39" ];
+            fedora = ["41" "40" "39" ];
         };
         version = "13";
-        vmTestv = { system?"x86_64-linux", distro?"debian", version?null, diskSize?"+1G", ... }@fattr : true;
-        vmTest = nix-vm-test.lib.${system}.${distro}.${version} {
+
+        vmTest = { system, distro, version  } : nix-vm-test.lib.${system}.${distro}.${version} {
 
           # This makes the guest-shared folder available in the test at /mnt/host-shared
 
@@ -62,15 +64,36 @@
             vm.succeed("apt-get -yq install /mnt/host-shared/hello.deb")
           '';
         };
+
+        vmTestv = { system?"x86_64-linux", distro?"debian", version?null, diskSize?"+1G", ... }@fattr :
+            let 
+                _distro = if versions ? ${distro} then distro else throw "Unsupported distro: ${ distro }";
+                _version = if isNull version then builtins.elemAt versions.${_distro} 0 else
+                    if builtins.elem version versions.${_distro} then version else 
+                        throw "Unsupported version ${version} for distro: ${ toString distro }";
+                var = builtins.trace "system=${system}; distro=${_distro} version=${_version}; " 
+                    vmTest { system = system; distro = _distro; version = _version; } ;
+               in var;
+
         in {
             # Run the sandboxed run with `nix flake check`
             checks.x86_64-linux.myTest = vmTest.sandboxed;
 
             packages.x86_64-linux = rec {
                 hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-                test-vm = vmTest.driver;
-                test-vm-interactive = vmTest.driverInteractive;
+                test-vm = (vmTestv {distro=distro;}).driver;
+                test-vm-interactive = (vmTestv {distro=distro;}).driverInteractive;
+                debian-test-vm = (vmTestv {distro="debian";}).driver;
+                debian-test-vm-interactive = (vmTestv {distro="debian";}).driverInteractive;
+                fedora-test-vm = (vmTestv {distro="fedora";}).driver;
+                fedora-test-vm-interactive = (vmTestv {distro="fedora";}).driverInteractive;
                 default = test-vm;
             };
+
+            debug = _: let var = builtins.trace "foo" "bar"; in var;
+            test-vm = vmTestv {distro=distro;};
+
+            inherit versions vmTestv;
+        
         };
 }
